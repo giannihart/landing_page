d0a0482ce3183c571bd1c25dde367043f89025ac
Gianni <giannimhart@gmail.com>
2025-03-09 22:50:18 -0700

    Fixed spelling in features section

--- src/components/features/Features.tsx
+++ src/components/features/Features.tsx
@@ -21,7 +21,7 @@ const features = [
   },
   {
     title: 'API Testing Playground',
-    description: 'Test every API before using it on your production enviornment.',
+    description: 'Test every API before using it on your production environment.',
     icon: (
       <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 4h16M4 4v8M20 4v8M7 12v4M17 12v4M7 16h10M12 16v4" />
